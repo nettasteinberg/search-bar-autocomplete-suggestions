c1fb08fdd65b705043191da287bfe0b87295ed92
Netta Steinberg <netta.steinberg@gmail.com>
2023-04-19 17:36:51 +0300

    Added search button click functionality and foundation for search bar autocomplete functionality

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const body = document.querySelector("body");
 const mainDiv = document.createElement("div");
 mainDiv.classList.add("mainDiv");
 
-// const form = document.createElement("form");
-// form.classList.add("form");
-
 const searchButton = document.createElement("button");
 searchButton.type = "submit";
 searchButton.classList.add("searchButton");
@@ -14,9 +11,28 @@ const input = document.createElement("input");
 input.type = "search";
 input.classList.add("input");
 
-
-// form.append(searchButton, input);
-
-// mainDiv.append(form);
 mainDiv.append(searchButton, input);
 body.append(mainDiv);
+
+const suggestions = [];
+
+searchButton.addEventListener("click", function(event) {
+    if (!input.value) {
+        return;
+    }
+    sessionStorage.setItem(input.value, "");
+    input.value = "";
+    input.focus();
+})
+
+input.addEventListener("keypress", function(event) {
+    suggestions = [];
+    const keys = Object.keys(sessionStorage);
+    const inputCurrentVale = input.value
+    keys.forEach(key => function() {
+        if (inputCurrentVale.length > key) {}
+        if (key.substring(inputCurrentVale.length) === input) {
+            suggestions.add(key);
+        }
+    })
+})
